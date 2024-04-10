a8ea9b041ffa0975473605f0501378019df62286
surgosan <sergy200118@gmail.com>
2024-04-09 23:20:32 -0400

    Fundamentals of Data Communications Project

---
+++ Fundamentals_of_Data_Communications/src/client_server_project/client.java
@@ -0,0 +1,32 @@
+package client_server_project;
+import java.io.*;
+import java.net.*;
+
+public class client {
+    public static void main(String[] args) {
+        final String serverAddress = "192.168.1.16"; // Replace this with the server machine's IP address
+        final int port = 8080;
+
+        try(Socket socket = new Socket(serverAddress, port);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
+
+            System.out.println("Successfully connected to server. \n Send a message or send `end` to terminate.");
+
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                String response = in.readLine();
+                System.out.println("Received response: " + response);
+                if (userInput.equals("end") || response.equals("dne")) {
+                    break;
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("An error was made. Could not start client: " + e);
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Fundamentals_of_Data_Communications/src/client_server_project/server.java
@@ -0,0 +1,39 @@
+package client_server_project;
+import java.io.*;
+import java.net.*;
+
+
+public class server {
+    public static void main(String[] args) {
+        final int portNumber = 8081;
+
+        try(ServerSocket serverSocket = new ServerSocket(portNumber)) {
+            System.out.println("Waiting for connection...");
+
+            while(true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("New client connected at: " + clientSocket);
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                String inLine;
+                while ((inLine = in.readLine()) != null) {
+                    if(inLine.equals("end")) {
+                        out.println("dne");
+                        break;
+                    }
+                    out.println(new StringBuilder(inLine).reverse().toString());
+                }
+
+                in.close();
+                out.close();
+                clientSocket.close();
+            }
+
+        } catch (IOException e) {
+            System.out.println("There was an error with server socket");
+            e.printStackTrace();
+        }
+    }
+}
